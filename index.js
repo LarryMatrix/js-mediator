9a8bc1a3ba236c39decf1e6396ccf7f7f4d7c54f
Lawrance Massanja <massanjal4@gmail.com>
2021-12-14 17:38:04 +0300

    initialization of js mediator

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const parser = require('body-parser');
+const mediatorUtils = require('openhim-mediator-utils');
+const apiConf = require('./config/config');
+const mediatorConfig = require('./config/mediator');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3400;
+let URL = mediatorConfig.config.hfr.url;
+let FACILITIES_URL = mediatorConfig.config.hfr.hfr_facilities;
+
+app.use(parser.json());
+
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+app.get('/mediator', (req, res) => {
+    const PARAMS = req.query;
+
+    axios.get(URL, {
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+        },
+        params: PARAMS
+    }).then((response) => {
+        res.send(response.data);
+    }).catch((error) => {
+        console.log(error);
+    }).then(() => {
+        console.log('executed');
+    });
+
+});
+
+app.get('/health-facilities', (req, res) => {
+    const PARAMS = req.query;
+    axios.get(FACILITIES_URL, {
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+        },
+        params: PARAMS
+    }).then((response) => {
+        res.send(response.data);
+        console.log('resp', response.data);
+    }).catch((error) => {
+        console.log('error');
+    }).then(() => {
+        console.log('executed');
+    });
+});
+
+app.post('/hrhis-mediator', (req, res) => {
+    payload = {};
+    payload.code = req.body.Fac_IdNumber;
+    payload.name = req.body.Name;
+    payload.shortName = req.body.Comm_FacName;
+    payload.openingDate = req.body.OpenedDate;
+    payload.coordinates = JSON.stringify([req.body.Latitude, req.body.Longitude]);
+    payload.active = req.body.OperatingStatus !== 'Operating';
+    payload.parent = {
+        code: req.body.Council_Code
+    };
+    payload.organisationUnitGroups = [
+        {
+            code: req.body.FacilityTypeGroupCode
+        },
+        {
+            code: req.body.FacilityTypeCode
+        },
+        {
+            code: req.body.OwnershipCode
+        },
+        {
+            code: req.body.OwnershipGroupCode
+        }
+    ];
+
+    res.send(payload);
+
+});
+
+
+mediatorUtils.registerMediator(apiConf.api, mediatorConfig, (error) => {
+    apiConf.api.urn = mediatorConfig.urn;
+
+    mediatorUtils.fetchConfig(apiConf.api, (err, newConfig) => {
+        console.info('Received initial config:', newConfig);
+        config = newConfig;
+        if (err) {
+            console.info('Failed to fetch initial config');
+            console.info(err.stack);
+            process.exit(1);
+        } else {
+            console.info('Successfully registered mediator!');
+            app.listen(PORT, () => {
+                console.log(`Mediator Listening from ${PORT}...`);
+
+                let configEmitter = mediatorUtils.activateHeartbeat(apiConf.api);
+                configEmitter.on('error', (error) => {
+                    console.error(error);
+                    console.error('an error occured while trying to activate heartbeat');
+                });
+                configEmitter.on('config', (newConfig) => {
+                    console.info('Received updated config:', newConfig);
+                    // set new config for mediator
+                    config = newConfig;
+                });
+            });
+        }
+    });
+});
